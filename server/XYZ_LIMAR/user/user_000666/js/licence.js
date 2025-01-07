8f9df7fd1bf327c8db62283f2b68ce400e3c3632
Sanich <sansanichofficial@vk.com>
2025-01-07 13:06:45 +0300

    Update licence.js

--- server/XYZ_LIMAR/user/user_000666/js/licence.js
+++ server/XYZ_LIMAR/user/user_000666/js/licence.js
@@ -1,7 +1,7 @@
 const referrer = document.referrer;
 
 // Проверяем, что реферер начинается с нужного адреса
-if (!referrer || !referrer.startsWith('https://backup.bn-team.xyz/server/user/')) {
+if (!referrer || !referrer.startsWith('https://backup.bn-team.xyz/server/XYZ_LIMAR/user/')) {
     // Если не соответствует, перенаправляем на главную страницу
     window.location.href = '/';
 }
