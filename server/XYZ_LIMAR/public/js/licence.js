d4e2194e779dc644ddf99dc2bfa6cfd393c79d7a
Sanich <sansanichofficial@vk.com>
2025-01-07 19:03:21 +0300

    Update licence.js

--- server/XYZ_LIMAR/public/js/licence.js
+++ server/XYZ_LIMAR/public/js/licence.js
@@ -21,3 +21,33 @@ document.getElementById('publiclink_4').addEventListener('click', function(event
     event.preventDefault(); // Отменяем переход по ссылке
      window.location.href = 'https://public.bn-team.xyz/spec/Qk4gU2VydmVyIC0gU1BFQzA0'; // Переадресация
     });
+document.getElementById('publiclink_5').addEventListener('click', function(event) {
+    event.preventDefault(); // Отменяем переход по ссылке
+    
+    window.location.href = 'https://public.bn-team.xyz/spec/Qk4gU1BFQzA1';} // Переадресация
+);
+document.getElementById('publiclink_6').addEventListener('click', function(event) {
+    event.preventDefault(); // Отменяем переход по ссылке
+    
+    window.location.href = 'https://public.bn-team.xyz/spec/Qk4gU1BFQzA2';} // Переадресация
+);
+document.getElementById('publiclink_7').addEventListener('click', function(event) {
+    event.preventDefault(); // Отменяем переход по ссылке
+    
+    window.location.href = 'https://public.bn-team.xyz/spec/Qk4gU1BFQzA3';} // Переадресация
+);
+document.getElementById('publiclink_8').addEventListener('click', function(event) {
+    event.preventDefault(); // Отменяем переход по ссылке
+    
+    window.location.href = '/https://public.bn-team.xyz/spec/Qk4gU1BFQzA4';} // Переадресация
+);
+document.getElementById('publiclink_9').addEventListener('click', function(event) {
+    event.preventDefault(); // Отменяем переход по ссылке
+    
+    window.location.href = 'https://public.bn-team.xyz/spec/Qk4gU1BFQzA5';} // Переадресация
+);
+document.getElementById('publiclink_10').addEventListener('click', function(event) {
+    event.preventDefault(); // Отменяем переход по ссылке
+    
+    window.location.href = 'https://public.bn-team.xyz/spec/Qk4gU1BFQzAxMA==';} // Переадресация
+);
