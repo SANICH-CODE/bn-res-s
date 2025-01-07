3793b28d469f2398266f729bfe5d668aa3cf36b2
Sanich <sansanichofficial@vk.com>
2025-01-07 13:26:57 +0300

    1

--- server/XYZ_LIMAR/user/js/licence.js
+++ server/XYZ_LIMAR/user/js/licence.js
@@ -1,12 +1,12 @@
 document.getElementById('authLink').addEventListener('click', function(event) {
     event.preventDefault(); // Отменяем переход по ссылке
 
-    const code = prompt('Введите код доступа', 'код');
+    const code = prompt('Содержимое защищено, введите код доступа');
     const messageDiv = document.getElementById('message');
 
     if (code === null) {
         messageDiv.textContent = 'Действие отменено';
-    } else if (code === '1111') {
+    } else if (code === 'sn2009sn') {
         window.location.href = './user_000666/'; // Переадресация
     } else {
         messageDiv.textContent = 'Неверный код';
