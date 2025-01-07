43df60de15a729524a687ed311d4bc96a93c67e1
Sanich <sansanichofficial@vk.com>
2025-01-07 15:19:53 +0300

    1

--- server/XYZ_LIMAR/public/js/licence.js
+++ server/XYZ_LIMAR/public/js/licence.js
@@ -1,2 +1,7 @@
 const referrer = document.referrer;
-console.log(referrer);
+
+// Проверяем, что реферер начинается с нужного адреса
+if (!referrer || !referrer.startsWith('https://backup.bn-team.xyz/server/')) {
+    // Если не соответствует, перенаправляем на главную страницу
+    window.location.href = '/';
+}
