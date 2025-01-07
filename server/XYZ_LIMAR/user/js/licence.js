5f0b68474a75e35ab0fd1b6380b5b46526aae867
Sanich <sansanichofficial@vk.com>
2025-01-07 13:01:00 +0300

    1

--- server/XYZ_LIMAR/user/js/licence.js
+++ server/XYZ_LIMAR/user/js/licence.js
@@ -1,1 +1,14 @@
-window.confirm('Вы хотите перейти на YouTube.com. Продолжить?');
+document.getElementById('authLink').addEventListener('click', function(event) {
+    event.preventDefault(); // Отменяем переход по ссылке
+
+    const code = prompt('Введите код доступа', 'код');
+    const messageDiv = document.getElementById('message');
+
+    if (code === null) {
+        messageDiv.textContent = 'Действие отменено';
+    } else if (code === '1111') {
+        window.location.href = '/user_000666/'; // Переадресация
+    } else {
+        messageDiv.textContent = 'Неверный код';
+    }
+});
