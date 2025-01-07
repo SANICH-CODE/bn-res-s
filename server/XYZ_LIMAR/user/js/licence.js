d494e4425d8c545d41ef5d2220d16ecb68b50487
Sanich <sansanichofficial@vk.com>
2025-01-07 18:28:46 +0300

    12

--- server/XYZ_LIMAR/user/js/licence.js
+++ server/XYZ_LIMAR/user/js/licence.js
@@ -7,8 +7,22 @@ document.getElementById('authLink').addEventListener('click', function(event) {
     if (code === null) {
         messageDiv.textContent = 'Действие отменено';
     } else if (code === 'sn2009sn') {
-        window.location.href = './user_000666/'; // Переадресация
+        window.location.href = './user_000666/'; // Переадресация для первого пользователя
+    } else if (code === 'aboba228') { // Замените 'anotherCode' на код для второго пользователя
+        window.location.href = './user_012579/'; // Переадресация для второго пользователя
     } else {
         messageDiv.textContent = 'Неверный код';
     }
 });
+const code = prompt('Введите ваш код');
+const messageDiv = document.getElementById('message');
+
+if (code === null) {
+    messageDiv.textContent = 'Действие отменено';
+} else if (code === 'sn2009sn') {
+    window.location.href = './user_000666/'; // Переадресация для первого пользователя
+} else if (code === 'aboba228') { // Замените 'anotherCode' на код для второго пользователя
+    window.location.href = './user_012579/'; // Переадресация для второго пользователя
+} else {
+    messageDiv.textContent = 'Неверный код';
+}
